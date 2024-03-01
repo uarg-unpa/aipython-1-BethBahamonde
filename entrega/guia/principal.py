3a0186c11159e186081246c785a4e882afb4ebaf
BethBahamonde <146486951+BethBahamonde@users.noreply.github.com>
2024-03-01 20:35:59 +0000

    practica_010324

---
+++ entrega/guia/principal.py
@@ -0,0 +1,15 @@
+print("Taller de AIPython P1 E2")
+#print("El", "taller", "es")
+print("Test",sep="\n", end="\nNueva Linea")
+
+print("Hola alumnos          ","es viernes",sep='-')
+print("Probando argumentos","soy el segundo argumento", end='*')
+print("Soy otra linea")
+print("soy seis")
+
+print("Regla", "De", "Oro", sep="-")    
+# Primero en las funciones, cuando nosotros lo invocamos, tiene que venir si o si los argumentos proporcionales y luego los argumentos claves
+
+print ("Taller de\"AIPython\" P1 E2")
+
+print('Una computadora puede ser llamada "inteligente" si logra engañar a una persona haciéndole creer que es un humano.')
